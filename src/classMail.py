f28b8950a60c763bd78383bb77f9d34a7444639a
MinhTran <20melios22@gmail.com>
2023-10-18 17:18:42 +0700

    commit lai

--- src/classMail.py
+++ src/classMail.py
@@ -218,7 +218,7 @@ class Mail:
         spinner.start()
         while True:
             self.fetch_mail()
-            if len(self.cmd_list) != 0:
+            if self.cmd_list:
                 spinner.stop()
                 # print(self.cmd_list)
                 self.process_command()
