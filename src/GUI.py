39074060baa6be197f3797a199d9a235353922f9
Thieunubenbodeadline <81817335+kggmt@users.noreply.github.com>
2023-11-18 22:18:16 +0700

    update GUI

--- src/GUI.py
+++ src/GUI.py
@@ -84,14 +84,13 @@ class Widget(customtkinter.CTkButton):
     __x = 3
     __y = 3
 
-    __m_master = ""
+    __m_master = None
     __sender_mail = ""
     __recive_mail = ""
     __reply_subject = ""
-    __subject = ""
+    __subject = []
     __date = ""
-    __attachment = ""
-
+    __attachment = []
     
     
     def attachment_event(self, command : str, attachment : str):
@@ -120,7 +119,7 @@ class Widget(customtkinter.CTkButton):
         
 
 
-    def button_event(self, m_master, sender_mail : str, recive_mail : str, subject : str, date : str, reply_subject : str, attachment : str):
+    def button_event(self, m_master, sender_mail : str, recive_mail : str, subject : [], date : str, reply_subject : str, attachment : []):
         for widget in m_master.widget_list:
             widget.pack_forget()
 
@@ -143,19 +142,22 @@ class Widget(customtkinter.CTkButton):
         self.date_zone = customtkinter.CTkButton(master=self.info_space, text=date, width=self.__WIDTH/4, height=20, fg_color="#f3defa", anchor="e", font=("Arial", 20), hover=0, text_color="black", border_width=0 )
         self.date_zone.place(x=self.__WIDTH/4 * 3 - 2 * self.__x, y=12)
 
-        self.subject_zone = customtkinter.CTkButton (master=m_master.widget_label, text="", width=self.__WIDTH, height=self.__HEIGHT/10 * 3, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=2, border_color="black", corner_radius=0)
-        self.subject_zone.pack(padx=5, pady=5)
-        self.subject_letter = customtkinter.CTkButton(master=self.subject_zone, text="Content:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0  )
-        self.subject_letter.place(x=3,y=12)
+        self.subject_zone = customtkinter.CTkScrollableFrame (master=m_master.widget_label,  width=self.__WIDTH - 20, height=170, fg_color="#f3defa", corner_radius=0, border_width=2)
+        self.subject_zone._scrollbar.configure(height=0)
+        self.subject_zone.pack()
 
-        self.subject_content = customtkinter.CTkButton(master=self.subject_zone, text=subject, width=self.__WIDTH - self.__WIDTH/4, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20), hover=0,text_color="black", border_width=0  )
-        self.subject_content.place(x=120 ,y=12)
+        self.subject_letter = customtkinter.CTkButton(master=self.subject_zone, text="Content:", width=40, height=20, fg_color="#f3defa", anchor="nw", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0)
+        self.subject_letter.pack(side="top", fill="x", expand=True)
+
+
+        for i in range(len(subject)):
+            customtkinter.CTkButton(master=self.subject_zone, text="> " + str(subject[i]), width=self.__WIDTH/10, height=20, fg_color="#f3defa", anchor="nw", font=("Arial", 20), hover=0,text_color="black", border_width=0).pack(side="top", fill = "x", expand=True)
         
         self.break_line = customtkinter.CTkButton(master=m_master.widget_label, text="", width=self.__WIDTH, height=7, fg_color="black", anchor="w",corner_radius=20, hover=0, border_width=0, border_color="black")
         self.break_line.pack(padx=2, pady=2)
 
-        self.reply_zone = customtkinter.CTkButton(master=m_master.widget_label, text="", width=self.__WIDTH, height=self.__HEIGHT/2 - 46, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=2, border_color="black", corner_radius=0)
-        self.reply_zone.pack(padx=5, pady=5)
+        self.reply_zone = customtkinter.CTkButton(master=m_master.widget_label, text="", width=self.__WIDTH, height=self.__HEIGHT/2 - 35 , fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=2, border_color="black", corner_radius=0)
+        self.reply_zone.pack(padx=5, pady=0)
 
         self.reply_from_letter = customtkinter.CTkButton(master=self.reply_zone, text="From:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0  )
         self.reply_from_letter.place(x=3,y=12)
@@ -166,34 +168,56 @@ class Widget(customtkinter.CTkButton):
         self.reply_subject_letter = customtkinter.CTkButton(master=self.reply_zone, text="Content:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0  )
         self.reply_subject_letter.place(x=3,y=50)
 
-        self.reply_subject = customtkinter.CTkButton(master=self.reply_zone, text=reply_subject, width=self.__WIDTH - self.__WIDTH/4, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20), hover=0,text_color="black", border_width= 0 )
+        self.reply_subject = customtkinter.CTkButton(master=self.reply_zone, text="reply_subject", width=self.__WIDTH - self.__WIDTH/4, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20), hover=0,text_color="black", border_width= 0 )
         self.reply_subject.place(x=100 ,y=50)
 
-        self.attachment_letter = customtkinter.CTkButton(master=self.reply_zone, text="Attachment:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0  )
-        if (attachment == ""):
-            self.attachment_letter.place(x=3,y=88)
-            customtkinter.CTkButton(master=self.reply_zone, text="None", width=self.__WIDTH/15, height=30, fg_color="#f3defa", anchor="w", font=("Arial", 20),text_color="black", hover=False).place(x=140 ,y=87)   
-            return
-        self.attachment_letter.place(x=3,y=94)
+        self.attachment_zone = customtkinter.CTkScrollableFrame(master=self.reply_zone, width=self.__WIDTH - 21, height=140, fg_color="#f3defa",corner_radius=0, border_width=2, border_color="black")
+        self.attachment_zone._scrollbar.configure(height=0)
+        self.attachment_zone.place(x=0,y=100)
+        self.attachment_letter = customtkinter.CTkButton(master=self.attachment_zone, text="Attachment:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black", border_width=0)
+        self.attachment_letter.grid(row=0, column=0, sticky="w")
+
+
         attachment_icon = customtkinter.CTkImage(Image.open("assets/attach.png"), size=(30,30))
-        self.new_attachment = attachment.split("\\")[-1]
-        self.attachment = customtkinter.CTkButton(master=self.reply_zone, text=self.new_attachment, width=self.__WIDTH/4, height=20, fg_color="#4c0f66", anchor="w", font=("Arial", 20),text_color="white", border_width= 2, corner_radius=0, border_color="black", image=attachment_icon, hover_color="#4c0f66", command=lambda: self.attachment_event(command=subject, attachment=attachment))
-        self.attachment.place(x=160 ,y=88)        
+
+        self.attach_space = [customtkinter.CTkButton] * 9
+        self.attachment = [customtkinter.CTkButton] * 9
         
+        row, col = (1, 0)
         
+        for i in range(len(attachment)):
+            self.new_attachment = str(attachment[i]).split("\\")[-1]
+            self.attach_space[i] = customtkinter.CTkButton(master=self.attachment_zone, text="", width=self.__WIDTH/5, height=50, fg_color="#f3defa", anchor="nw", font=("Arial", 20), hover=0, border_width=0, corner_radius=0)
+            self.attach_space[i].grid(row=row, column=col)  # Place the first widget
+
+            self.attachment[i] = customtkinter.CTkButton(master=self.attach_space[i], text=self.new_attachment, width=self.__WIDTH/10, height=20, fg_color="#4c0f66", anchor="nw", font=("Arial", 20),text_color="white", hover=0, border_width=2,  border_color="black", image=attachment_icon, command=lambda i=i: self.attachment_event(command=subject[i], attachment=attachment[i]))
+            self.attachment[i].grid(row=row, column=col) 
+            col += 1  # Increment column count for the next widget
+            if col == 4:  # If three widgets are placed, move to the next row
+                row += 1
+                col = 0
+
+      
+
     def add_widget(self):
 
         self.mail_lb = customtkinter.CTkButton(master=self, text=self.__sender_mail, width=self.__WIDTH/3 - self.__x, height=self.__HEIGHT/10 - self.__y, fg_color="#f3defa", anchor="w", font=("Arial", 20), text_color="black", hover=0, corner_radius=0, command=lambda: self.button_event(m_master=self.__m_master, sender_mail=self.__sender_mail, recive_mail=self.__recive_mail, subject=self.__subject, reply_subject=self.__reply_subject, date=self.__date, attachment=self.__attachment))
         self.mail_lb.place(x=2,y=2)
+        self.content = "" 
+        for i in range(len(self.__subject)):
+            self.content += str(self.__subject[i])
+            self.content += " "
 
-        self.subject_lb = customtkinter.CTkButton(master=self, text=self.__subject, width=self.__WIDTH/3 - self.__x, height=self.__HEIGHT/10 - self.__y, fg_color="#f3defa", anchor="w", font=("Arial", 20), text_color="black", hover=0, corner_radius=0, command=lambda: self.button_event(m_master=self.__m_master, sender_mail=self.__sender_mail, recive_mail=self.__recive_mail, subject=self.__subject, reply_subject=self.__reply_subject, date=self.__date, attachment=self.__attachment))
+        self.content = self.content[:30] + "..."
+
+        self.subject_lb = customtkinter.CTkButton(master=self, text=self.content, width=self.__WIDTH/3 - self.__x, height=self.__HEIGHT/10 - self.__y, fg_color="#f3defa", anchor="w", font=("Arial", 20), text_color="black", hover=0, corner_radius=0, command=lambda: self.button_event(m_master=self.__m_master, sender_mail=self.__sender_mail, recive_mail=self.__recive_mail, subject=self.__subject, reply_subject=self.__reply_subject, date=self.__date, attachment=self.__attachment))
         self.subject_lb.place(x=self.__WIDTH/3 + self.__x, y=2)
 
         self.date_lb = customtkinter.CTkButton(master=self, text=self.__date, width=self.__WIDTH/3 - self.__x, height=self.__HEIGHT/10 - self.__y, fg_color="#f3defa", anchor="e", font=("Arial", 20), text_color="black", hover=0, corner_radius=0, command=lambda: self.button_event(m_master=self.__m_master, sender_mail=self.__sender_mail, recive_mail=self.__recive_mail, subject=self.__subject, date=self.__date,reply_subject=self.__reply_subject, attachment=self.__attachment))
         self.date_lb.place(x=self.__WIDTH/3 * 2 - 2 * self.__x, y=2)
 
         
-    def __init__(self, m_master, sender_mail : str, recive_mail : str, subject : str, date : str, attachment : str, reply_subject : str, **kwargs):   
+    def __init__(self, m_master, sender_mail : str, recive_mail : str, subject : [], date : str, attachment : [], reply_subject : str, **kwargs):   
         super().__init__( **kwargs)
        
         self.configure(text="", width=m_master.APP_WIDTH - self.__x, height=m_master.APP_HEIGHT/10 , fg_color="#f3defa", anchor="w",corner_radius=0, hover=0, border_width=2, border_color="black")
@@ -212,12 +236,13 @@ class Widget(customtkinter.CTkButton):
         
 class Mail_page(customtkinter.CTkScrollableFrame):
     __info_lb = None
-    def add_widget(self, master, sender_mail : str, recive_mail : str, subject : str, date : str, reply_subject : str, attachment : str):
+    def add_widget(self, master, sender_mail : str, recive_mail : str, subject : [], date : str, reply_subject : str, attachment : []):
         self.widget = Widget(master=self, m_master=master, sender_mail=sender_mail, recive_mail=recive_mail, subject=subject, date=date,reply_subject=reply_subject, attachment=attachment)
         if (self.widget not in master.widget_list):
             master.widget_list.append(self.widget)
 
         self.widget.pack(after=self.__info_lb, padx=5, pady=5)
+        
 
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
@@ -226,15 +251,15 @@ class Mail_page(customtkinter.CTkScrollableFrame):
         self.__info_lb = customtkinter.CTkButton(master=self,text="", width=master.APP_WIDTH - 13, height=master.APP_HEIGHT/10 , fg_color="#f3defa", anchor="w",corner_radius=0, hover=0, border_width=3, border_color="black")
         self.__info_lb.pack(pady = 1)
 
-        self.from_letter = customtkinter.CTkButton(master=self.__info_lb, text="Email:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black" )
+        self.from_letter = customtkinter.CTkButton(master=self.__info_lb, text="Email", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black" )
         self.from_letter.place(x=3,y=12)
 
-        self.subject_letter = customtkinter.CTkButton(master=self.__info_lb, text="Content:", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black"  )
+        self.subject_letter = customtkinter.CTkButton(master=self.__info_lb, text="Content", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0,text_color="black"  )
         self.subject_letter.place(x=master.APP_WIDTH/3 ,y=12)
 
         self.date_letter = customtkinter.CTkButton(master=self.__info_lb, text="Date", width=40, height=20, fg_color="#f3defa", anchor="w", font=("Arial", 20, "bold"), hover=0, text_color="black" )
         self.date_letter.place(x=master.APP_WIDTH/3 * 3 - 80,y=12)
-        
+
         
        
             
@@ -246,7 +271,7 @@ class App(customtkinter.CTk):
     sender_email = ""
     widget_list = []
   
-    def add_new_mail(self, sender_mail : str, subject : str, date : str, reply_subject : str,  attachment : str):
+    def add_new_mail(self, sender_mail : str, subject : [], date : str, reply_subject : str,  attachment : []):
         self.widget_label.add_widget(master=self, sender_mail=sender_mail, subject=subject, recive_mail=self.self_email, date=date, attachment=attachment, reply_subject=reply_subject)
         
     
@@ -263,9 +288,16 @@ class App(customtkinter.CTk):
         self.widget_label = Mail_page(master=self)
         self.widget_label.place(x=0, y=0)
 
+
+
 if __name__ == "__main__":
     # Splash_Screen()
     app = App()
-    for i in range(5):
-        app.add_new_mail(sender_mail="SenderEmail", subject="Subject", date="Date", reply_subject="ReplySubject", attachment="Files\\Processes.txt")
+
+    cmd_list = ["screenshot a", "webcam b", "keylog 10", "screenshot a", "webcam b", "keylog 10", "screenshot a", "webcam b", "keylog 10"]
+    cmd_list = [i.split(" ") for i in cmd_list]
+    
+    attachment_list = ["Files\\Pictures\\a.png","Files\\Pictures\\b.png", "Files\\Keylog.txt", "Files\\Pictures\\a.png","Files\\Pictures\\b.png", "Files\\Keylog.txt", "Files\\Pictures\\a.png","Files\Pictures\\b.png", "Files\\Keylog.txt"]
+    for i in range(10):
+        app.add_new_mail(sender_mail="SenderEmail", subject=cmd_list, date="Date", reply_subject="ReplySubject", attachment=attachment_list)
     app.mainloop()
